0b1c299081969e4cbdc88a02b041029854f1def5
AndyIsCool5463 <www.andyiscool@outlook.com>
2018-11-30 18:14:37 -0800

    Git Pages

--- docs/git_resources/scripts/index.js
+++ docs/git_resources/scripts/index.js
@@ -4,3 +4,6 @@ function home() {
 function site() {
   window.location.replace("https://official-website.glitch.me");
 }
+function github() {
+  window.location.replace("https://github.com/andyiscool5463/aicofficial");
+}
